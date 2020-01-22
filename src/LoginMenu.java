db64b60a9ceb85148fd1a5ec912dd0873e6a2b0b
TheEternalShenron <rachit.gla_cs18@gla.ac.in>
2020-01-22 16:52:59 +0530

    changes in LOGIN MENU completed

--- src/LoginMenu.java
+++ src/LoginMenu.java
@@ -2,24 +2,34 @@ public class LoginMenu {
 
     private String id;
     private String pswrd;
+    private int count;
 
-    public void setCredentials(String id, String pswrd) {
-//        Class.forName("com.mysql.cj.jdbc.Driver");
+    public boolean checkID(String id) {
         this.id = id;
+        boolean response = false;
+
+        String countstr = Encryption.MD5(id);
+        if (countstr.equals("63a9f0ea7bb98050796b649e85481845")) {
+            this.count = 0;
+            response = true;
+        } else if (countstr.equals("93f725a07423fe1c889f448b33d21f46")) {
+            count = 1;
+            response = true;
+        }
+
+        return response;
+    }
+
+    public boolean checkPswrd(String pswrd) {
         this.pswrd = pswrd;
+        boolean response = false;
+        String countstr = Encryption.SHA1(pswrd);
 
-//        Encryption.MD5(id);
-//        Encryption.SHA1(pswrd);
+        if (countstr.equals("dc76e9f0c0006e8f919e0c515c66dbba3982f785") && count == 0) response = true;
 
+        else if (countstr.equals("23524be9dba14bc2f1975b37f95c3381771595c8") && count == 1) response = true;
 
-    }
-
-    public String getID(String id) {
-        return id;
-    }
-
-    public String getPswrd(String pswrd) {
-        return pswrd;
+        return response;
     }
 
 }
--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,25 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-//        Encryption en = new Encryption();
-//        long n = 1234567891234567890L;
-        System.out.println(Encryption.MD5("cfcd208495d565ef66e7dff9f98764da"));
-        System.out.println(Encryption.SHA1("83a3717a23947e2e0d308d28cd4be755a807f58e"));
+        LoginMenu lm = new LoginMenu();
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter LOGIN ID");
+        String inpid = sc.nextLine().toLowerCase();
+
+        System.out.println("Enter LOGIN PSWRD");
+        String inppwd = sc.nextLine();
+
+        System.out.println("Welcome Sir!");
+
+//        try {
+//            Runtime.getRuntime().exec("clear");
+//        } catch (IOException e) {
+//
+//        }
+
+        if (lm.checkID(inpid) && lm.checkPswrd(inppwd)) System.out.println(Encryption.MD5("java"));
+
     }
 }
