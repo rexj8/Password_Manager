5e622a7d7d3d31b52315006330cdde95c70b7ad5
TheEternalShenron <rachit.gla_cs18@gla.ac.in>
2020-01-23 22:27:06 +0530

    init of MAIN MENU insert method

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,46 @@
 import java.util.Scanner;
 
 public class Main {
+
     public static void main(String[] args) {
         LoginMenu lm = new LoginMenu();
         Scanner sc = new Scanner(System.in);
 
+        System.out.println("================");
+        System.out.println("   LOGIN MENU   ");
+        System.out.println("================");
+
         System.out.println("Enter LOGIN ID");
-        String inpid = sc.nextLine().toLowerCase();
-
+        String inpLGNID = sc.nextLine().toLowerCase();
         System.out.println("Enter LOGIN PSWRD");
-        String inppwd = sc.nextLine();
+        String inpLGNPWD = sc.nextLine();
 
-        if (lm.checkID(inpid) && lm.checkPswrd(inppwd)) System.out.println("Welcome Sir!");
+        if (lm.checkID(inpLGNID) && lm.checkPswrd(inpLGNPWD)) {
+            System.out.println("Welcome Sir!");
+
+            System.out.println("===============");
+            System.out.println("   MAIN MENU   ");
+            System.out.println("===============");
+
+            System.out.println("1. ADD your data");
+            System.out.println("2. REMOVE some data");
+            System.out.println("3. OPEN url");
+            System.out.println("4. EXIT");
+
+            int inpMENU = sc.nextInt();
+
+            if (inpMENU == 1) {
+                System.out.print("URL-");
+                String inpURL = sc.next();
+
+                System.out.print("USER NAME-");
+                String inpUsrNAME = sc.next();
+
+                System.out.print("PASSWORD-");
+                String inpPWD = sc.next();
+            } else if (inpMENU == 4) System.exit(0);
+            else System.out.println("Wrong input!!");
+        }
         else System.out.println("Your ID or PASSWORD did not match");
 
     }
