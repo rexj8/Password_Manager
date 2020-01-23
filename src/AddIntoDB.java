cb5bf41a23e9bd437f2eb82d70abd2a1a4d7f561
TheEternalShenron <rachit.gla_cs18@gla.ac.in>
2020-01-23 22:43:47 +0530

    Class AddIntoDB added and init of AddIntoDB.add()

---
+++ src/AddIntoDB.java
@@ -0,0 +1,8 @@
+public class AddIntoDB {
+
+    public static void add(String url, String Uname, String pwd) {
+
+
+    }
+
+}
--- src/Main.java
+++ src/Main.java
@@ -6,22 +6,21 @@ public class Main {
         LoginMenu lm = new LoginMenu();
         Scanner sc = new Scanner(System.in);
 
-        System.out.println("================");
-        System.out.println("   LOGIN MENU   ");
-        System.out.println("================");
-
-        System.out.println("Enter LOGIN ID");
-        String inpLGNID = sc.nextLine().toLowerCase();
-        System.out.println("Enter LOGIN PSWRD");
-        String inpLGNPWD = sc.nextLine();
-
-        if (lm.checkID(inpLGNID) && lm.checkPswrd(inpLGNPWD)) {
+//        System.out.println("================");
+//        System.out.println("   LOGIN MENU   ");
+//        System.out.println("================");
+//
+//        System.out.println("Enter LOGIN ID");
+//        String inpLGNID = sc.nextLine().toLowerCase();
+//        System.out.println("Enter LOGIN PSWRD");
+//        String inpLGNPWD = sc.nextLine();
+//
+//        if (lm.checkID(inpLGNID) && lm.checkPswrd(inpLGNPWD))
+        {
             System.out.println("Welcome Sir!");
-
             System.out.println("===============");
             System.out.println("   MAIN MENU   ");
             System.out.println("===============");
-
             System.out.println("1. ADD your data");
             System.out.println("2. REMOVE some data");
             System.out.println("3. OPEN url");
@@ -38,10 +37,12 @@ public class Main {
 
                 System.out.print("PASSWORD-");
                 String inpPWD = sc.next();
+
+                AddIntoDB.add(inpURL, inpUsrNAME, inpPWD);
             } else if (inpMENU == 4) System.exit(0);
             else System.out.println("Wrong input!!");
         }
-        else System.out.println("Your ID or PASSWORD did not match");
+//        else System.out.println("Your ID or PASSWORD did not match");
 
     }
 }
